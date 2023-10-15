e429854a13f984348bc5dfad14442dfc4ac37d7f
5nik7 <nicflip86@gmail.com>
2023-10-15 14:28:59 -0700

    new

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,6 @@
+local Config = require("lazyvim.config")
+local Util = require("lazyvim.util")
+
 return {
   {
     "stevearc/aerial.nvim",
@@ -44,5 +47,52 @@ return {
     keys = {
       { "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
     },
+  },
+
+  -- Telescope integration
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = function()
+      Util.on_load("telescope.nvim", function()
+        require("telescope").load_extension("aerial")
+      end)
+    end,
+    keys = {
+      {
+        "<leader>ss",
+        "<cmd>Telescope aerial<cr>",
+        desc = "Goto Symbol (Aerial)",
+      },
+    },
+  },
+
+  -- lualine integration
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_c, {
+        "aerial",
+        sep = " ", -- separator between symbols
+        sep_icon = "", -- separator between icon and symbol
+
+        -- The number of symbols to render top-down. In order to render only 'N' last
+        -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+        -- be used in order to render only current symbol.
+        depth = 5,
+
+        -- When 'dense' mode is on, icons are not rendered near their symbols. Only
+        -- a single icon that represents the kind of current symbol is rendered at
+        -- the beginning of status line.
+        dense = false,
+
+        -- The separator to be used to separate symbols in dense mode.
+        dense_sep = ".",
+
+        -- Color the symbol icons.
+        colored = true,
+      })
+    end,
   },
 }
--- lua/plugins/mini-starter.lua
+++ lua/plugins/mini-starter.lua
@@ -1,4 +1,10 @@
+-- start screen
 return {
+  -- disable alpha
+  { "goolord/alpha-nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+
+  -- enable mini.starter
   {
     "echasnovski/mini.starter",
     version = false, -- wait till new 0.7.0 release to put it back on semver
@@ -12,32 +18,32 @@ return {
    ║  ║\  ║║  ╚══╗║  ╚═╝  ║████╔╝ ██║██║║  ║  ║  ║
    ╚══╝╚══╝╚═════╝╚═══════╝╚═══╝  ╚═╝╚═╝╚══╝  ╚══╝
 ]]
-      local pad = string.rep(" ", 20)
+      local pad = string.rep(" ", 22)
       local new_section = function(name, action, section)
         return { name = name, action = action, section = pad .. section }
       end
 
       local starter = require("mini.starter")
-    --stylua: ignore
-    local config = {
-      evaluate_single = true,
-      header = logo,
-      items = {
-        new_section("Find file",       "Telescope find_files",                                   "Telescope"),
-        new_section("Recent files",    "Telescope oldfiles",                                     "Telescope"),
-        new_section("Grep text",       "Telescope live_grep",                                    "Telescope"),
-        new_section("Config",          "lua require('lazyvim.util').telescope.config_files()()", "Config"),
-        new_section("Extras",          "LazyExtras",                                             "Config"),
-        new_section("Lazy",            "Lazy",                                                   "Config"),
-        new_section("New file",        "ene | startinsert",                                      "Built-in"),
-        new_section("Quit",            "qa",                                                     "Built-in"),
-        new_section("Session restore", [[lua require("persistence").load()]],                    "Session"),
-      },
-      content_hooks = {
-        starter.gen_hook.adding_bullet(pad .. "░ ", false),
-        starter.gen_hook.aligning("center", "center"),
-      },
-    }
+      --stylua: ignore
+      local config = {
+        evaluate_single = true,
+        header = logo,
+        items = {
+          new_section("Find file",       "Telescope find_files",                                   "Telescope"),
+          new_section("Recent files",    "Telescope oldfiles",                                     "Telescope"),
+          new_section("Grep text",       "Telescope live_grep",                                    "Telescope"),
+          new_section("Config",          "lua require('lazyvim.util').telescope.config_files()()", "Config"),
+          new_section("Extras",          "LazyExtras",                                             "Config"),
+          new_section("Lazy",            "Lazy",                                                   "Config"),
+          new_section("New file",        "ene | startinsert",                                      "Built-in"),
+          new_section("Quit",            "qa",                                                     "Built-in"),
+          new_section("Session restore", [[lua require("persistence").load()]],                    "Session"),
+        },
+        content_hooks = {
+          starter.gen_hook.adding_bullet(pad .. "░ ", false),
+          starter.gen_hook.aligning("center", "center"),
+        },
+      }
       return config
     end,
     config = function(_, config)
