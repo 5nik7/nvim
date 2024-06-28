68384f4f77aad9fd8afa34e295737a8386e5dced
Nick Jensen <nickflip86@gmail.com>
2024-06-27 23:42:30 -0700

    Update @ 2024-06-27 23:42

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,7 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 vim.g.lazygit_config = true
 
 vim.g.lazyvim_statuscolumn = {
-  folds_open = false,  -- show fold sign when fold is open
+  folds_open = false, -- show fold sign when fold is open
   folds_githl = false, -- highlight fold sign with git sign color
 }
 
@@ -21,31 +21,31 @@ vim.g.trouble_lualine = true
 
 local opt = vim.opt
 
-opt.autowrite = true                                    -- Enable auto write
+opt.autowrite = true -- Enable auto write
 opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
 
--- if not vim.env.SSH_TTY then
---   opt.clipboard = "unnamedplus"
--- end
+if not vim.env.SSH_TTY then
+  opt.clipboard = "unnamedplus"
+end
 
--- vim.g.clipboard = {
---   name = "win32yank-wsl",
---   copy = {
---     ["+"] = "win32yank.exe -i --crlf",
---     ["*"] = "win32yank.exe -i --crlf",
---   },
---   paste = {
---     ["+"] = "win32yank.exe -o --lf",
---     ["*"] = "win32yank.exe -o --lf",
---   },
---   cache_enabled = 0,
--- }
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
 
 opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 2  -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true    -- Confirm to save changes before exiting modified buffer
+opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true  -- Use spaces instead of tabs
+opt.expandtab = true -- Use spaces instead of tabs
 opt.fillchars = {
   foldopen = "",
   foldclose = "",
@@ -59,42 +59,42 @@ opt.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
 opt.formatoptions = "jcroqlnt" -- tcqj
 opt.grepformat = "%f:%l:%c:%m"
 opt.grepprg = "rg --vimgrep"
-opt.ignorecase = true      -- Ignore case
+opt.ignorecase = true -- Ignore case
 opt.inccommand = "nosplit" -- preview incremental substitute
-opt.laststatus = 3         -- global statusline
-opt.linebreak = true       -- Wrap lines at convenient points
-opt.list = true            -- Show some invisible characters (tabs...
-opt.mouse = "a"            -- Enable mouse mode
-opt.number = true          -- Print line number
-opt.pumblend = 10          -- Popup blend
-opt.pumheight = 10         -- Maximum number of entries in a popup
-opt.relativenumber = true  -- Relative line numbers
-opt.scrolloff = 4          -- Lines of context
+opt.laststatus = 3 -- global statusline
+opt.linebreak = true -- Wrap lines at convenient points
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.relativenumber = true -- Relative line numbers
+opt.scrolloff = 4 -- Lines of context
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true      -- Round indent
-opt.shiftwidth = 2         -- Size of an indent
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false       -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8      -- Columns of context
-opt.signcolumn = "yes"     -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true       -- Don't ignore case with capitals
-opt.smartindent = true     -- Insert indents automatically
+opt.showmode = false -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8 -- Columns of context
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
 opt.spelllang = { "en" }
 opt.spelloptions:append("noplainbuffer")
-opt.splitbelow = true                         -- Put new windows below current
+opt.splitbelow = true -- Put new windows below current
 opt.splitkeep = "screen"
-opt.splitright = true                         -- Put new windows right of current
+opt.splitright = true -- Put new windows right of current
 opt.statuscolumn = [[%!v:lua.require'lazyvim.util'.ui.statuscolumn()]]
-opt.tabstop = 2                               -- Number of spaces tabs count for
-opt.termguicolors = true                      -- True color support
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
 opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
 opt.undofile = true
 opt.undolevels = 10000
-opt.updatetime = 200               -- Save swap file and trigger CursorHold
-opt.virtualedit = "block"          -- Allow cursor to move where there is no text in visual block mode
+opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5                -- Minimum window width
-opt.wrap = false                   -- Disable line wrap
+opt.winminwidth = 5 -- Minimum window width
+opt.wrap = false -- Disable line wrap
 
 if vim.fn.has("nvim-0.10") == 1 then
   opt.smoothscroll = true
@@ -111,8 +111,7 @@ vim.g.markdown_recommended_style = 0
 
 local powershell_options = {
   shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-  shellcmdflag =
-  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
   shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
   shellquote = "",
