c4184a368c5a9f8c6e76c5a46a4eb5da11bf87af
Nick Jensen <nickflip86@gmail.com>
2024-09-10 00:19:46 -0700

    Update @ 09-10-2024 00:19

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,13 +17,6 @@ local logo = {
  │ ╚═╝  ╚═══╝ ╚══════╝  ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝ │
  └─────────────────────────────────────────────────────────┘
  ]],
-  simple = [[
-_|      _|  _|_|_|_|    _|_|    _|      _|  _|_|_|  _|      _|
-_|_|    _|  _|        _|    _|  _|      _|    _|    _|_|  _|_|
-_|  _|  _|  _|_|_|    _|    _|  _|      _|    _|    _|  _|  _|
-_|    _|_|  _|        _|    _|    _|  _|      _|    _|      _|
-_|      _|  _|_|_|_|    _|_|        _|      _|_|_|  _|      _|
- ]],
 }
 local line = [[
 
@@ -114,7 +107,7 @@ local days_of_week = {
 -- ███████╗███████║   ██║   ██║   ██║██████╔╝██║  ██║███████║ ╚████╔╝⠀
 -- ╚════██║██╔══██║   ██║   ██║   ██║██╔══██╗██║  ██║██╔══██║  ╚██╔╝⠀⠀
 -- ███████║██║  ██║   ██║   ╚██████╔╝██║  ██║██████╔╝██║  ██║   ██║⠀⠀⠀
--- ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝⠀⠀⠀
+-- ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝⠀⠀⠀P
 --   ]],
 --   Sunday = [[
 -- ███████╗██╗   ██╗███╗   ██╗██████╗  █████╗ ██╗   ██╗
@@ -131,18 +124,6 @@ return {
     opts = {
       config = {
         header = vim.split(string.rep("\n", 15) .. logo.block .. "\n\n", "\n"),
-        -- center = {
-        --   { action = 'lua LazyVim.pick()()', desc = " Find File", icon = " ", key = "f" },
-        --   { action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
-        --   { action = 'lua LazyVim.pick("oldfiles")()', desc = " Recent Files", icon = " ", key = "r" },
-        --   { action = 'lua LazyVim.pick("live_grep")()', desc = " Find Text", icon = " ", key = "g" },
-        --   { action = 'lua LazyVim.pick.config_files()()', desc = " Config", icon = " ", key = "c" },
-        --   { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-        --   { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
-        --   { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-        --   { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit", icon = " ", key = "q" },
-        --   { desc = " " },
-        -- },
       },
     },
   },
