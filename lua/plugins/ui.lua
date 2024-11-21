4a9a6a3fc0780aa605d54f37be105268a63392fa
Nick Jensen <nickflip86@gmail.com>
2024-11-20 20:41:07 -0800

    Update @ 11-20-2024 20:41

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,7 +3,7 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
-        width = 45,
+        width = 40,
         preset = {
           -- Used by the `header` section
           header = [[
@@ -16,7 +16,6 @@ return {
         },
         sections = {
           { section = "header", padding = 2 },
-          -- { section = "terminal", cmd = "cfonts NVIM -f block -c blueBright,gray", padding = 0 },
           -- { section = "keys", gap = 1, padding = 2 },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
           { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
