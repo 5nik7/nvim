8696f7fa565e4c2036edc356f75ac090b1ad3170
njen <contact@njen.io>
2025-01-19 15:15:56 -0800

    Update @ 01-19-2025 15:15

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,7 @@
 -- vim.g.loaded_ruby_provider = 0
 -- vim.g.loaded_node_provider = 0
 --
--- if vim.fn.has("win32") == 1 then
---   LazyVim.terminal.setup("pwsh")
--- end
+
+if vim.fn.has("win32") == 1 then
+  LazyVim.terminal.setup("pwsh")
+end
