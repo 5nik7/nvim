de69e9b02dcf6d5a163c81077d7ec9f74da45e29
njen <contact@njen.io>
2025-01-25 00:58:11 -0800

    Update @ 01-25-2025 00:58

--- lua/util/wal.lua
+++ lua/util/wal.lua
@@ -1,1 +1,25 @@
-C:/Users/njen/.cache/wal/colors.lua
+local M = {}
+
+M.wal = {
+    background = "#151D38",
+    foreground = "#d6e9ff",
+    cursor     = "#5500FF",
+    color0     = "#2E3C5B",
+    color1     = "#EB6F92",
+    color2     = "#00FFCC",
+    color3     = "#FFB78D",
+    color4     = "#477EFF",
+    color5     = "#9896FF",
+    color6     = "#00FFFB",
+    color7     = "#d6e9ff",
+    color8     = "#374B72",
+    color9     = "#EB6F92",
+    color10    = "#8FF8B5",
+    color11    = "#F2C996",
+    color12    = "#5792FF",
+    color13    = "#A7A6FF",
+    color14    = "#30F1EA",
+    color15    = "#d6e9ff",
+}
+
+return M
