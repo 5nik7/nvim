be8198b7c0ca2a2d4e40928ff553daab12008a08
njen <contact@njen.io>
2025-03-24 12:46:41 -0700

    Update @ 03-24-2025 12:46

--- lua/util/wal.lua
+++ lua/util/wal.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.wal = {
-<<<<<<< HEAD
   background = "#1E1E2E",
   foreground = "#CDD6F4",
   cursor = "#F5E0DC",
@@ -20,28 +19,7 @@ M.wal = {
   color12 = "#89B4FA",
   color13 = "#F5C2E7",
   color14 = "#94E2D5",
-  color15 = "#A6ADC8",
-=======
-    background = "#1E1E2E",
-    foreground = "#CDD6F4",
-    cursor     = "#F5E0DC",
-    color0     = "#45475A",
-    color1     = "#F38BA8",
-    color2     = "#A6E3A1",
-    color3     = "#F9E2AF",
-    color4     = "#89B4FA",
-    color5     = "#F5C2E7",
-    color6     = "#94E2D5",
-    color7     = "#BAC2DE",
-    color8     = "#585B70",
-    color9     = "#F38BA8",
-    color10    = "#A6E3A1",
-    color11    = "#F9E2AF",
-    color12    = "#89B4FA",
-    color13    = "#F5C2E7",
-    color14    = "#94E2D5",
-    color15    = "#A6ADC8",
->>>>>>> 3e3cbf3 (Update @ 03-23-2025 17:11)
+  color15 = "#A6ADC8"
 }
 
 return M
