df056ee12b60d4de1e18059814620fef34dfb142
5nik7 <nickflip86@gmail.com>
2025-05-22 02:30:20 -0700

    wsl

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,18 @@
 if vim.fn.has("win32") == 1 then
   LazyVim.terminal.setup("pwsh")
 end
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
