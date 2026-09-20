daf948ab18156e2fff82ba8d9a43620b6ec383fd
njen <contact@njen.io>
2026-09-19 21:11:38 -0700

    Shorten dashboard filenames to fit available width

--- lua/util/ui.lua
+++ lua/util/ui.lua
@@ -8,15 +8,28 @@ function M.format_dashboard_file(item, ctx)
   local fname = vim.fs.normalize(vim.fn.fnamemodify(item.file, ":~")):gsub("\\", "/")
 
   -- Shorten directory components first.
-  if ctx.width and #fname > ctx.width then
+  if ctx.width and vim.fn.strdisplaywidth(fname) > ctx.width then
     fname = vim.fn.pathshorten(fname)
   end
 
-  -- If it is still too long, discard the directory rather than
-  -- truncating the filename.
-  if ctx.width and #fname > ctx.width then
+  -- If it is still too long, discard the directory first.
+  if ctx.width and vim.fn.strdisplaywidth(fname) > ctx.width then
     local file = vim.fn.fnamemodify(fname, ":t")
     fname = "…/" .. file
+  end
+
+  -- Keep the filename's tail when even the shortened path will not fit.
+  if ctx.width and vim.fn.strdisplaywidth(fname) > ctx.width then
+    local file = vim.fn.fnamemodify(fname, ":t")
+    if vim.fn.strdisplaywidth(file) <= ctx.width then
+      fname = file
+    else
+      local prefix = (".."):sub(1, math.max(0, ctx.width))
+      while vim.fn.strdisplaywidth(file) > ctx.width - #prefix and file ~= "" do
+        file = vim.fn.strcharpart(file, 1)
+      end
+      fname = prefix .. file
+    end
   end
 
   local dir, file = fname:match("^(.*)/(.+)$")
